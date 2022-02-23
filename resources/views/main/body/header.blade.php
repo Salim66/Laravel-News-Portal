c0837350151db9a44ee711559b54ccf19ff64fed
Salim66 <salimhasanriad8@gmail.com>
2022-02-23 11:34:28 +0600

    Frontend category and subcategory added

--- resources/views/main/body/header.blade.php
+++ resources/views/main/body/header.blade.php
@@ -21,198 +21,37 @@
                    <li class="nav-item">
                       <a href="#" class="nav-link active">
                       Home
-                      <i class='bx bx-chevron-down'></i>
-                      </a>
-                      <ul class="dropdown-menu">
-                         <li class="nav-item">
-                            <a href="index.html" class="nav-link">
-                            Home one
-                            </a>
-                         </li>
-                         <li class="nav-item">
-                            <a href="index-2.html" class="nav-link active">
-                            Home two
-                            </a>
-                         </li>
-                         <li class="nav-item">
-                            <a href="index-3.html" class="nav-link">
-                            Home three
-                            </a>
-                         </li>
-                         <li class="nav-item">
-                            <a href="index-4.html" class="nav-link">
-                            Home four
-                            </a>
-                         </li>
-                      </ul>
-                   </li>
-                   <li class="nav-item">
-                      <a href="politics.html" class="nav-link">
-                      Politics
                       </a>
                    </li>
-                   <li class="nav-item">
-                      <a href="business.html" class="nav-link">
-                      Business
-                      </a>
-                   </li>
-                   <li class="nav-item">
-                      <a href="technology.html" class="nav-link">
-                      Tech
-                      </a>
-                   </li>
-                   <li class="nav-item">
-                      <a href="sport.html" class="nav-link">
-                      Sport
-                      </a>
-                   </li>
-                   <li class="nav-item">
-                      <a href="culture.html" class="nav-link">
-                      Culture
-                      </a>
-                   </li>
-                   <li class="nav-item">
-                      <a href="video.html" class="nav-link">
-                      Video
-                      </a>
-                   </li>
+@php
+    $categories = DB::table('categories')->orderBy('id', 'ASC')->get();
+@endphp
+
+                   @foreach($categories as $cat)
                    <li class="nav-item">
                       <a href="#" class="nav-link">
-                      News
+                      {{ $cat->category_en }}
                       <i class='bx bx-chevron-down'></i>
                       </a>
                       <ul class="dropdown-menu">
+
+                    @php
+                        $subcategories = DB::table('subcategories')->where('category_id', $cat->id)->get();
+                    @endphp
+
+                        @foreach($subcategories as $subcat)
                          <li class="nav-item">
                             <a href="news.html" class="nav-link">
-                            News
+                            {{ $subcat->subcategory_en }}
                             </a>
                          </li>
-                         <li class="nav-item">
-                            <a href="news-right-sidebar.html" class="nav-link">
-                            News right sidebar
-                            </a>
-                         </li>
-                         <li class="nav-item">
-                            <a href="news-left-sidebar.html" class="nav-link">
-                            News left sidebar
-                            </a>
-                         </li>
-                         <li class="nav-item">
-                            <a href="news-without-sidebar.html" class="nav-link">
-                            News without sidebar
-                            </a>
-                         </li>
-                         <li class="nav-item">
-                            <a href="#" class="nav-link">
-                            Post layout
-                            <i class='bx bx-chevron-right'></i>
-                            </a>
-                            <ul class="dropdown-menu">
-                               <li class="nav-item">
-                                  <a href="news-details-1.html" class="nav-link">
-                                  Post layout 1
-                                  </a>
-                               </li>
-                               <li class="nav-item">
-                                  <a href="news-details-2.html" class="nav-link">
-                                  Post layout 2
-                                  </a>
-                               </li>
-                               <li class="nav-item">
-                                  <a href="news-details-3.html" class="nav-link">
-                                  Post layout 3
-                                  </a>
-                               </li>
-                            </ul>
-                         </li>
+                         @endforeach
+
                       </ul>
                    </li>
-                   <li class="nav-item">
-                      <a href="#" class="nav-link">
-                      Pages
-                      <i class='bx bx-chevron-down'></i>
-                      </a>
-                      <ul class="dropdown-menu">
-                         <li class="nav-item">
-                            <a href="author.html" class="nav-link">
-                            Author
-                            </a>
-                         </li>
-                         <li class="nav-item">
-                            <a href="about-us.html" class="nav-link">
-                            About us
-                            </a>
-                         </li>
-                         <li class="nav-item">
-                            <a href="our-team.html" class="nav-link">
-                            Our team
-                            </a>
-                         </li>
-                         <li class="nav-item">
-                            <a href="fashion.html" class="nav-link">
-                            Fashion
-                            </a>
-                         </li>
-                         <li class="nav-item">
-                            <a href="entertainment.html" class="nav-link">
-                            Entertainment
-                            </a>
-                         </li>
-                         <li class="nav-item">
-                            <a href="lifestyle.html" class="nav-link">
-                            Lifestyle
-                            </a>
-                         </li>
-                         <li class="nav-item">
-                            <a href="international.html" class="nav-link">
-                            International
-                            </a>
-                         </li>
-                         <li class="nav-item">
-                            <a href="#" class="nav-link">
-                            Account
-                            <i class='bx bx-chevron-down'></i>
-                            </a>
-                            <ul class="dropdown-menu">
-                               <li class="nav-item">
-                                  <a href="login.html" class="nav-link">
-                                  Login
-                                  </a>
-                               </li>
-                               <li class="nav-item">
-                                  <a href="register.html" class="nav-link">
-                                  Register
-                                  </a>
-                               </li>
-                            </ul>
-                         </li>
-                         <li class="nav-item">
-                            <a href="error-404.html" class="nav-link">
-                            404 error
-                            </a>
-                         </li>
-                         <li class="nav-item">
-                            <a href="terms-of-service.html" class="nav-link">
-                            Terms of service
-                            </a>
-                         </li>
-                         <li class="nav-item">
-                            <a href="privacy-policy.html" class="nav-link">
-                            Privacy policy
-                            </a>
-                         </li>
-                         <li class="nav-item">
-                            <a href="coming-soon.html" class="nav-link">
-                            Coming soon
-                            </a>
-                         </li>
-                         <li class="nav-item">
-                            <a href="contact.html" class="nav-link">
-                            Contact
-                            </a>
-                         </li>
-                      </ul>
-                   </li>
+                   @endforeach
+
+
                 </ul>
                 <div class="others-options d-flex align-items-center">
                    <div class="option-item">
