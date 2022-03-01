5e30a59c17e4b6fb7a1ab909b952cb0e34340536
Salim66 <salimhasanriad8@gmail.com>
2022-03-01 18:00:04 +0600

    Admin Home Dynamic

--- resources/views/admin/body/sidebar.blade.php
+++ resources/views/admin/body/sidebar.blade.php
@@ -130,7 +130,7 @@
       <li class="nav-item menu-items">
         <a class="nav-link" data-toggle="collapse" href="#posts" aria-expanded="false" aria-controls="posts">
           <span class="menu-icon">
-            <i class="mdi mdi-security"></i>
+            <i class="mdi mdi-file-document-box"></i>
           </span>
           <span class="menu-title">Posts</span>
           <i class="menu-arrow"></i>
@@ -247,39 +247,7 @@
 
       @endif
 
-      <li class="nav-item menu-items">
-        <a class="nav-link" href="pages/forms/basic_elements.html">
-          <span class="menu-icon">
-            <i class="mdi mdi-playlist-play"></i>
-          </span>
-          <span class="menu-title">Form Elements</span>
-        </a>
-      </li>
-      <li class="nav-item menu-items">
-        <a class="nav-link" href="pages/tables/basic-table.html">
-          <span class="menu-icon">
-            <i class="mdi mdi-table-large"></i>
-          </span>
-          <span class="menu-title">Tables</span>
-        </a>
-      </li>
 
-      <li class="nav-item menu-items">
-        <a class="nav-link" href="pages/charts/chartjs.html">
-          <span class="menu-icon">
-            <i class="mdi mdi-chart-bar"></i>
-          </span>
-          <span class="menu-title">Charts</span>
-        </a>
-      </li>
-      <li class="nav-item menu-items">
-        <a class="nav-link" href="pages/icons/mdi.html">
-          <span class="menu-icon">
-            <i class="mdi mdi-contacts"></i>
-          </span>
-          <span class="menu-title">Icons</span>
-        </a>
-      </li>
 
       <li class="nav-item menu-items">
         <a class="nav-link" href="http://www.bootstrapdash.com/demo/corona-free/jquery/documentation/documentation.html">
@@ -289,5 +257,6 @@
           <span class="menu-title">Documentation</span>
         </a>
       </li>
+
     </ul>
   </nav>
--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -4,27 +4,35 @@
 
 <div class="content-wrapper">
     <div class="row">
-      <div class="col-12 grid-margin stretch-card">
-        <div class="card corona-gradient-card">
-          <div class="card-body py-0 px-0 px-sm-3">
-            <div class="row align-items-center">
-              <div class="col-4 col-sm-3 col-xl-2">
-                <img src="{{ asset('backend/') }}/assets/images/dashboard/Group126@2x.png" class="gradient-corona-img img-fluid" alt="">
-              </div>
-              <div class="col-5 col-sm-7 col-xl-8 p-0">
-                <h4 class="mb-1 mb-sm-0">Want even more features?</h4>
-                <p class="mb-0 font-weight-normal d-none d-sm-block">Check out our Pro version with 5 unique layouts!</p>
-              </div>
-              <div class="col-3 col-sm-2 col-xl-2 pl-0 text-center">
-                <span>
-                  <a href="https://www.bootstrapdash.com/product/corona-admin-template/" target="_blank" class="btn btn-outline-light btn-rounded get-started-btn">Upgrade to PRO</a>
-                </span>
+        <div class="col-12 grid-margin stretch-card">
+          <div class="card corona-gradient-card">
+            <div class="card-body py-0 px-0 px-sm-3">
+              <div class="row align-items-center">
+                <div class="col-4 col-sm-3 col-xl-2">
+                  <img src="{{ asset('backend/') }}/assets/images/dashboard/Group126@2x.png" class="gradient-corona-img img-fluid" alt="">
+                </div>
+                <div class="col-5 col-sm-7 col-xl-8 p-0">
+                  <h4 class="mb-1 mb-sm-0">Welcome To Chandlee News</h4>
+                </div>
+                <div class="col-3 col-sm-2 col-xl-2 pl-0 text-center">
+                  <span>
+                    <a href="{{ url('/') }}" target="_blank" class="btn btn-outline-light btn-rounded get-started-btn">Visit Frontend ?</a>
+                  </span>
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
     </div>
+
+
+    @php
+        $category = DB::table('categories')->get();
+        $subcategory = DB::table('subcategories')->get();
+        $post = DB::table('posts')->get();
+        $ads = DB::table('ads')->get();
+    @endphp
+
     <div class="row">
       <div class="col-xl-3 col-sm-6 grid-margin stretch-card">
         <div class="card">
@@ -32,8 +40,8 @@
             <div class="row">
               <div class="col-9">
                 <div class="d-flex align-items-center align-self-start">
-                  <h3 class="mb-0">$12.34</h3>
-                  <p class="text-success ml-2 mb-0 font-weight-medium">+3.5%</p>
+                  <h3 class="mb-0">{{ count($category) }}</h3>
+                  <p class="text-success ml-2 mb-0 font-weight-medium">Category</p>
                 </div>
               </div>
               <div class="col-3">
@@ -42,7 +50,7 @@
                 </div>
               </div>
             </div>
-            <h6 class="text-muted font-weight-normal">Potential growth</h6>
+            <h6 class="text-muted font-weight-normal">Category</h6>
           </div>
         </div>
       </div>
@@ -52,17 +60,17 @@
             <div class="row">
               <div class="col-9">
                 <div class="d-flex align-items-center align-self-start">
-                  <h3 class="mb-0">$17.34</h3>
-                  <p class="text-success ml-2 mb-0 font-weight-medium">+11%</p>
+                  <h3 class="mb-0">{{ count($subcategory) }}</h3>
+                  <p class="text-info ml-2 mb-0 font-weight-medium">SubCategory</p>
                 </div>
               </div>
               <div class="col-3">
-                <div class="icon icon-box-success">
+                <div class="icon icon-box-info">
                   <span class="mdi mdi-arrow-top-right icon-item"></span>
                 </div>
               </div>
             </div>
-            <h6 class="text-muted font-weight-normal">Revenue current</h6>
+            <h6 class="text-muted font-weight-normal">SubCategory</h6>
           </div>
         </div>
       </div>
@@ -72,8 +80,8 @@
             <div class="row">
               <div class="col-9">
                 <div class="d-flex align-items-center align-self-start">
-                  <h3 class="mb-0">$12.34</h3>
-                  <p class="text-danger ml-2 mb-0 font-weight-medium">-2.4%</p>
+                  <h3 class="mb-0">{{ count($post) }}</h3>
+                  <p class="text-danger ml-2 mb-0 font-weight-medium">Post</p>
                 </div>
               </div>
               <div class="col-3">
@@ -82,7 +90,7 @@
                 </div>
               </div>
             </div>
-            <h6 class="text-muted font-weight-normal">Daily Income</h6>
+            <h6 class="text-muted font-weight-normal">Post</h6>
           </div>
         </div>
       </div>
@@ -92,17 +100,17 @@
             <div class="row">
               <div class="col-9">
                 <div class="d-flex align-items-center align-self-start">
-                  <h3 class="mb-0">$31.53</h3>
-                  <p class="text-success ml-2 mb-0 font-weight-medium">+3.5%</p>
+                  <h3 class="mb-0">{{ count($ads) }}</h3>
+                  <p class="text-warning ml-2 mb-0 font-weight-medium">Advertisement</p>
                 </div>
               </div>
               <div class="col-3">
-                <div class="icon icon-box-success ">
+                <div class="icon icon-box-warning ">
                   <span class="mdi mdi-arrow-top-right icon-item"></span>
                 </div>
               </div>
             </div>
-            <h6 class="text-muted font-weight-normal">Expense current</h6>
+            <h6 class="text-muted font-weight-normal">Advertisement</h6>
           </div>
         </div>
       </div>
